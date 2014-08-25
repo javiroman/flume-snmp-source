4f82a18b3e206260fbbc26a5e2c9bb665d4c8baa
Javi Roman <jroman.espinar@gmail.com>
2014-08-25 13:32:06 +0200

    resorting code

--- src/main/java/org/apache/flume/source/snmp/SNMPTrapSource.java
+++ src/main/java/org/apache/flume/source/snmp/SNMPTrapSource.java
@@ -1,4 +1,4 @@
-/***************************************************************
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,96 +6,141 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *  
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- ****************************************************************/
-package org.apache.flume.source.snmp;
+ */
 
-import java.io.IOException;
+/*
+ *  The pollable source will get regularly polled by the source runner asking it 
+ *  to generate events. Nevertheless this is a event driven source which is 
+ *  responsible for generating the events itself and normally does this 
+ *  in response to some event happening, in this case the SNMP Trap.
+ */
+package org.apache.flume.source;
 
+import java.net.InetSocketAddress;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.flume.ChannelException;
 import org.apache.flume.Context;
+import org.apache.flume.CounterGroup;
 import org.apache.flume.Event;
-import org.apache.flume.EventDeliveryException;
-import org.apache.flume.PollableSource;
+import org.apache.flume.EventDrivenSource;
 import org.apache.flume.conf.Configurable;
-import org.apache.flume.event.SimpleEvent;
-import org.apache.flume.source.AbstractSource;
+import org.apache.flume.conf.Configurables;
+import org.apache.flume.source.SyslogUtils;
+import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.channel.socket.oio.OioDatagramChannelFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.snmp4j.smi.UdpAddress;
+public class SNMPTrap AbstractSource
+      implements EventDrivenSource, Configurable {
 
-public class SNMPTrapSource extends AbstractSource implements 
-        Configurable, PollableSource {
+  private int port;
+  private int maxsize = 1 << 16; // 64k is max allowable in RFC 5426
+  private String host = null;
+  private Channel nettyChannel;
+  private Map<String, String> formaterProp;
 
-    private static final Logger LOG = LoggerFactory.getLogger(SNMPTrapSource.class);
+  private static final Logger logger = LoggerFactory
+      .getLogger(SNMPTrap.class);
 
-    private String bindAddress;
-    private String bindPort;
+  private CounterGroup counterGroup = new CounterGroup();
 
-    private static final String DEFAULT_PORT = "162";
-    private static final String DEFAULT_BIND = "127.0.0.1";
+  public class syslogHandler extends SimpleChannelHandler {
+
+    private SyslogUtils syslogUtils = new SyslogUtils(true);
+
+    public void setFormater(Map<String, String> prop) {
+      syslogUtils.addFormats(prop);
+    }
 
     @Override
-	public void configure(Context context) {
-        /*
-         * Default is to listen on UDP port 162 on all IPv4 interfaces. 
-         * Since 162 is a privileged port, snmptrapd must typically be run as root. 
-         * Or change to non-privileged port > 1024.
-         */
-        bindAddress = context.getString("bind", DEFAULT_BIND);
-        bindPort = context.getString("port", DEFAULT_PORT);
-	}
-
-    private class Startup extends Thread {
-        public void run() {
-              try {
-                  SNMPTrap snmp4jTrapReceiver = new SNMPTrap();
-                    try {
-                        snmp4jTrapReceiver.listen(new UdpAddress(bindAddress + "/" + bindPort));
-                    }
-                    catch (IOException e) {
-                        LOG.warn("Error in Listening for Trap");
-                        LOG.warn("Exception Message = ", e.getMessage());
-                    }
-                } catch (Exception e) {
-                        LOG.warn("Scribe failed", e);
-                }
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent mEvent) {
+      try {
+        syslogUtils.setEventSize(maxsize);
+        Event e = syslogUtils.extractEvent((ChannelBuffer)mEvent.getMessage());
+        if (e == null) {
+          return;
         }
+        getChannelProcessor().processEvent(e);
+        counterGroup.incrementAndGet("events.success");
+      } catch (ChannelException ex) {
+        counterGroup.incrementAndGet("events.dropped");
+        logger.error("Error writting to channel", ex);
+        return;
+      }
+    }
+  }
+
+  @Override
+  public void start() {
+    // setup Netty server
+    ConnectionlessBootstrap serverBootstrap = new ConnectionlessBootstrap
+        (new OioDatagramChannelFactory(Executors.newCachedThreadPool()));
+    final syslogHandler handler = new syslogHandler();
+    handler.setFormater(formaterProp);
+    serverBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+      @Override
+      public ChannelPipeline getPipeline() {
+       return Channels.pipeline(handler);
+      }
+     });
+
+    if (host == null) {
+      nettyChannel = serverBootstrap.bind(new InetSocketAddress(port));
+    } else {
+      nettyChannel = serverBootstrap.bind(new InetSocketAddress(host, port));
     }
 
-    @Override
-    public void start() {
-        // Initialize the connection to the external client
-         Startup startupThread = new Startup();
-         startupThread.start();
+    super.start();
+  }
 
-        super.start();
+  @Override
+  public void stop() {
+    logger.info("Syslog UDP Source stopping...");
+    logger.info("Metrics:{}", counterGroup);
+    if (nettyChannel != null) {
+      nettyChannel.close();
+      try {
+        nettyChannel.getCloseFuture().await(60, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        logger.warn("netty server stop interrupted", e);
+      } finally {
+        nettyChannel = null;
+      }
     }
 
-    @Override
-    public void stop() {
-        // Disconnect from external client and do any additional cleanup
-        super.stop();
-    }
+    super.stop();
+  }
 
-    /*
-     * The runner of a PollableSource invokes the Source‘s process() method. 
-     * The process() method should check for new data and store it into the 
-     * Channel as Flume Events.
-     */
-    @Override
-	public Status process() throws EventDeliveryException {
+  @Override
+  public void configure(Context context) {
+    Configurables.ensureRequiredNonNull(
+        context, SyslogSourceConfigurationConstants.CONFIG_PORT);
 
-		return Status.READY;
-	}
+    port = context.getInteger(SyslogSourceConfigurationConstants.CONFIG_PORT);
+    host = context.getString(SyslogSourceConfigurationConstants.CONFIG_HOST);
+
+  }
+
 }
-
